c9cc1a6835432fd00e23fbdb173b319f670a4e72
itsyaboikris <kristoff_riley@outlook.com>
2022-12-04 19:54:52 -0400

    added button functionality

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -27,17 +27,17 @@ export function StoreItem({id, name, price, imgUrl}: StoreItemProps) {
 				<div className="mt-auto">
 					{
 						quantity === 0 ? (
-							<Button className="w-100">+ Add To Cart</Button>
+							<Button className="w-100" onClick={() => increaseCartQuantity(id)} >+ Add To Cart</Button>
 						) : (
 							<div className="d-flex align-items-center flex-column" style={{ gap: "0.5rem" }} >
 								<div className="d-flex align-items-center justify-content-center" style={{ gap: "0.5rem" }}>
-									<Button>-</Button>
+									<Button onClick={() => decreaseCartQuantity(id)} >-</Button>
 									<div>
 										<span className="fs-3">{quantity}</span> in cart
 									</div>
-									<Button>+</Button>
+									<Button onClick={() => increaseCartQuantity(id)} >+</Button>
 								</div>
-								<Button variant="danger">Remove</Button>
+								<Button variant="danger" onClick={() => removeFromCart(id)}>Remove</Button>
 							</div>	
 						)
 					}
